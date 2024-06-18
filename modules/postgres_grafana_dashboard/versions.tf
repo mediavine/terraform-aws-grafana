ea4266972c9678eac99e830cc4c41cafcf81a8c5
Mykol Benning <mykol@mediavine.com>
2024-06-18 11:16:50 -0400

    Add modules for postgres datasource and postgres dashboard

---
+++ modules/postgres_grafana_dashboard/versions.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.0"
+    }
+    grafana = {
+      source  = "grafana/grafana"
+      version = "2.8.1"
+    }
+  }
+}
+
+# provider "aws" {
+# }
+
+provider "grafana" {
+  url  = "https://monitoring.mv-ops.com/"
+  auth = var.grafana_api_token
+}
