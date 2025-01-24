f6055e1108f64fa634aed01535ea8c6690804672
Brandon Johnson <brandon.johnson@mediavine.com>
2025-01-24 10:26:39 -0500

    Add X-Ray resource for Grafana Data Source

--- modules/aws_grafana_datasource/main.tf
+++ modules/aws_grafana_datasource/main.tf
@@ -10,3 +10,16 @@ resource "grafana_data_source" "cloudwatch" {
     assumeRoleArn = aws_iam_role.this.arn
   })
 }
+
+resource "grafana_data_source" "xray" {
+  count = var.enable_xray ? 1 : 0
+
+  type = "xray"
+  name = "${var.grafana_data_source_name}-xray"
+
+  json_data_encoded = jsonencode({
+    defaultRegion = var.region
+    authType      = "grafana_assume_role"
+    assumeRoleArn = aws_iam_role.this.arn
+  })
+}
