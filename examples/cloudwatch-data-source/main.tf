95d2e990d960e3fc5a691efb497b5afb883e2ba5
Keith Miller <36780654+Keimille@users.noreply.github.com>
2025-01-27 16:05:51 -0500

    adds prom datasource (#34)

--- examples/cloudwatch-data-source/main.tf
+++ examples/cloudwatch-data-source/main.tf
@@ -1,8 +1,7 @@
 module "this" {
-  source = "../../modules/aws_grafana_datasource"
-
+  source                   = "../../modules/aws_grafana_datasource"
+  type                     = "cloudwatch"
   region                   = var.region
   grafana_data_source_name = var.grafana_data_source_name
-  enable_cloudwatch        = true
-  grafana_api_token        = "sometokenhere"
+  grafana_cloud_api_token  = "sometokenhere"
 }
--- modules/aws_grafana_datasource/variables.tf
+++ modules/aws_grafana_datasource/variables.tf
@@ -10,22 +10,20 @@ variable "grafana_data_source_name" {
   description = "value of the grafana data source name"
 }
 
-variable "enable_cloudwatch" {
-  type        = bool
-  default     = false
+# details on the 'can' function: https://developer.hashicorp.com/terraform/language/functions/can
+variable "type" {
+  type        = string
   description = "control whether to enable cloudwatch related resources are created"
+  validation {
+    condition     = can(regex("cloudwatch|grafana-amazonprometheus-datasource", var.type))
+    error_message = "Type must be either cloudwatch or grafana-amazonprometheus-datasource."
+  }
 }
 
 variable "grafana_cloud_api_token" {
   type        = string
   default     = ""
   description = "value of the grafana cloud api token"
-}
-
-variable "enable_amazon_prometheus" {
-  type        = bool
-  default     = false
-  description = "control whether to enable amazon prometheus related resources are created"
 }
 
 variable "enable_xray" {
@@ -37,9 +35,17 @@ variable "enable_xray" {
 variable "grafana_aws_account_id" {
   type        = string
   description = "value of the grafana aws account id"
+  default     = ""
 }
 
 variable "grafana_aws_external_id" {
   type        = string
   description = "value of the grafana aws external id"
+  default     = ""
+}
+
+variable "amp_workspace_url" {
+  type        = string
+  default     = ""
+  description = "value of the Amazon Managed Service for Prometheus workspace url"
 }
